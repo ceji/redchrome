0791f224d74d81bdbefa8361bbfeef9d1207c52d
chicha <cjimenez@voyageprive.com>
2015-05-18 14:00:51 +0200

    JS formating code

--- ext_script.js
+++ ext_script.js
@@ -1,30 +1,29 @@
 console.log("ext_script.js");
 
+$(document).ready(function ()
+{
+    var token = '';
 
-
-$(document).ready(function() {	
-
-var token = '';
-
-function setHeader(xhr) {
-  xhr.setRequestHeader('Authorization', token);
-}
+    function setHeader(xhr)
+    {
+        xhr.setRequestHeader('Authorization', token);
+    }
 
     $.ajax({
-        url: "https://redmine.bovpg.net/projects.json",
-		crossDomain: true,
-		dataType: 'jsonp',
-		type: 'GET',
-		beforeSend: setHeader
-    }).then(function(data) {
-		html = "<select>";
-		$.each(data.projects, function () {
-			html += ("<option value='" + this.id + "'>" + this.name + "</option>") ;
-		});
-		html += "</select>";
-		$('#projects').append(html);
-		
-		
-        
+        url        : "https://redmine.bovpg.net/projects.json",
+        crossDomain: true,
+        dataType   : 'jsonp',
+        type       : 'GET',
+        beforeSend : setHeader
+    }).then(function (data)
+    {
+        html = "<select>";
+        $.each(data.projects, function ()
+        {
+            html += ("<option value='" + this.id + "'>" + this.name + "</option>");
+        });
+        html += "</select>";
+        $('#projects').append(html);
+
     });
 });
